d53717df0d4e95d3db9e26bf35df8639c206a8d0
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-06-14 12:33:18 +0530

    Create securitygroup.tf

---
+++ securitygroup.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "morning-ssh-http" {
+  name        = "morning-ssh-http"
+  description = "allow ssh and http traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
