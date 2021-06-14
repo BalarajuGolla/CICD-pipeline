4441849f3c337cd90346a2e687fffbba54b424e8
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-06-14 11:11:27 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "tfvm" {
-  ami = "ami-0885b1f6bd170450c"
+  ami = "ami-06a0b4e3b7eb7a300"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.websg.id ]
   user_data = <<-EOF
