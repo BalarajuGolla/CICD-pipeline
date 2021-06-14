80627f9ba7be9f22267ab02904b3211261e5a556
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-06-14 15:25:14 +0530

    Update securitygroup.tf
    removed 443

--- securitygroup.tf
+++ securitygroup.tf
@@ -10,13 +10,6 @@ resource "aws_security_group" "demo-sg" {
   }
 
   ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
