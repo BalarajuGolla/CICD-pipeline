6d78db6384bc5c7a4a9314ecf3b6bfe7f088fb08
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-06-14 10:44:12 +0530

    Create variable.tf
    variable file

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIAXETNQAOYPLM5BJZY"
+  }
+variable "secret_key" {
+  default = "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"
+  }
+variable "region" {
+  default = "us-east-1"
+  }
