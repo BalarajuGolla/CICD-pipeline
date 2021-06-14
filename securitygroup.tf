c93c90cf7416ad78b063ba43d4db5a81628bc322
BalarajuGolla <43969489+BalarajuGolla@users.noreply.github.com>
2021-06-14 15:23:19 +0530

    Update securitygroup.tf
    updated security to enable port number 443

--- securitygroup.tf
+++ securitygroup.tf
@@ -9,6 +9,12 @@ resource "aws_security_group" "demo-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 80
